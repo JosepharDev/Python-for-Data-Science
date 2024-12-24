84098cfc4e299458635ea65648797fa3a6156e97
JosepharDev <josephardev@gmail.com>
2024-12-24 16:41:15 +0100

    as

---
+++ 00/ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["hello", "World"]
+ft_tuple = ("Hello", "morocco")
+ft_set = {"Hello", "Bengrire"}
+ft_dict = {"Hello": "1337"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
